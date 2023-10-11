a4413c7ffd2c24cebf61e602f858900dc062e88c
MikixIT <michael.46@live.it>
2023-10-11 18:54:25 +0200

    starting project with Vite

---
+++ poke-quiz/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div> <h1 className='text-blue-800 w-full h-screen flex items-center text-7xl'>Hello Tailwind with REACT</h1></div>
+  )
+}
+
+export default App
