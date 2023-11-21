2a51a1c2af9e8fcfb0d14a4f11b1c882ff66e51f
MikixIT <michael.46@live.it>
2023-11-21 17:27:44 +0100

    upload assets/pika img

--- poke-quiz/src/components/PokemonCard.jsx
+++ poke-quiz/src/components/PokemonCard.jsx
@@ -2,7 +2,7 @@ export default function CardPokemon() {
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-red-600 dark:border-red-600">
                 <a href="#">
-            <img className="rounded-t-lg" src="https://www.thesun.co.uk/wp-content/uploads/2018/10/NINTCHDBPICT000253615121.jpg" alt="Pika" />
+            <img className="rounded-t-lg" src="src/assets/pika.webp" alt="Pika" />
                 </a>
                 <div className="p-5">
                     <a href="#">
