1ec3ca31d0276837300df6aed5884ea852eab228
MikixIT <michael.46@live.it>
2023-11-22 20:04:50 +0100

    adding style tailwind

--- poke-quiz/src/components/WelcomePage.jsx
+++ poke-quiz/src/components/WelcomePage.jsx
@@ -4,7 +4,7 @@ export default function WelcomePage() {
   return (
     <div className="app">
       <h1>Welcome</h1>
-      <button>Play!</button>
+      <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Play</button>
     </div>
     
   )
