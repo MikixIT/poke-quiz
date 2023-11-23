119d4d8a0c8e7f42b3e8d1b76c5ce54553a699a6
MikixIT <michael.46@live.it>
2023-11-23 18:00:47 +0100

    fix css style img card, upload my icon and logo created by me and set on html doc index

--- poke-quiz/src/components/PokemonCard.jsx
+++ poke-quiz/src/components/PokemonCard.jsx
@@ -5,11 +5,15 @@ export default function PokemonCard({ name, image }) {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-red-600 dark:border-red-600">
       <a href="#">
-        <img className="rounded-t-lg" src={image} alt={name} />
+        <img
+          className="rounded-t-lg object-cover h-48 w-96"
+          src={image}
+          alt={name}
+        />
       </a>
       <div className="p-5">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
             {name}
           </h5>
         </a>
