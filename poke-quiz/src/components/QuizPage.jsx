19b06eb28e97a071d504a69da220bb48108e1594
MikixIT <michael.46@live.it>
2023-11-28 18:26:43 +0100

    set more pokemon can load from api and fix some tailwind in welcome page, adding rainbow color animated to text

--- poke-quiz/src/components/QuizPage.jsx
+++ poke-quiz/src/components/QuizPage.jsx
@@ -2,23 +2,47 @@ import PokemonCard from "./PokemonCard";
 import { useState, useEffect } from "react";
 
 export default function QuizPage() {
-  const [pokemonData, setPokemonData] = useState(null);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/2")
-      .then((response) => response.json())
-      .then((data) => setPokemonData(data));
+    const getRandomPokemon = async () => {
+      //array con quantità di card da creare
+      const randomPokemonNumbers = Array.from(
+        { length: 5 },
+        () => Math.floor(Math.random() * 1000) + 1
+      ); // Genera 3 numeri casuali da 1 a 1000
+      const pokemonPromises = randomPokemonNumbers.map((number) =>
+        fetch(`https://pokeapi.co/api/v2/pokemon/${number}`).then((response) =>
+          response.json()
+        )
+      );
+      const pokemonData = await Promise.all(pokemonPromises);
+      setPokemonData(pokemonData);
+      setLoading(false);
+    };
+
+    getRandomPokemon();
   }, []);
 
-  if (!pokemonData) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
-  const { name, sprites } = pokemonData;
-
   return (
     <div className="App">
-      <PokemonCard name={name} image={sprites.front_default} />
+      <div className="max-w-screen-lg mx-auto">
+        <h1 className="text-center text-5xl font-bold mb-8">Pokemon Quiz</h1>
+        <div className="grid grid-cols-3 gap-4 p-4">
+          {pokemonData.map((pokemon) => (
+            <PokemonCard
+              key={pokemon.id}
+              name={pokemon.name}
+              image={pokemon.sprites.front_default}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- poke-quiz/src/components/WelcomePage.jsx
+++ poke-quiz/src/components/WelcomePage.jsx
@@ -1,11 +1,18 @@
-
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
   return (
-    <div className="app">
-      <h1>Welcome</h1>
-      <Link to="/Quiz"> <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Play</button></Link>
+    <div className="app grid-cols-1 m-10 p-10 ">
+      <h1 className="text-4xl font-bold animate-rainbow m-20">Welcome</h1>
+      <Link to="/Quiz">
+        <img
+          src="/poke-quiz/public/poke-quiz-logo(createdByMikixiT).svg"
+          className="logo "
+        />
+        <button className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+          Play
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
