da23fd73f2bedadedab3d53d9e417b98fbdef534
MikixIT <michael.46@live.it>
2023-12-11 22:30:39 +0100

    adding variable and props on pokemoncard component for make work onlick function

--- poke-quiz/src/components/PokemonCard.jsx
+++ poke-quiz/src/components/PokemonCard.jsx
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types";
 //raw.githubuserc†ontent.com/PokeAPI/sprites/master/sprites/pokemon/329.png
-export default function PokemonCard({ name, image }) {
+export default function PokemonCard({ name, image, onClick }) {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-red-600 dark:border-red-600">
+    <div
+      className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-red-600 dark:border-red-600"
+      onClick={onClick}
+    >
       <div className="flex justify-center w-100 h-40 cursor-pointer">
         <img className="object-cover p-1 items-center" src={image} alt={name} />
       </div>
@@ -16,4 +19,5 @@ export default function PokemonCard({ name, image }) {
 PokemonCard.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
