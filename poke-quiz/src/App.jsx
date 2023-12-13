77a3304c875dff26d66c49ce92149d4f2748a8e7
MikixIT <michael.46@live.it>
2023-12-13 03:29:06 +0100

    trying to deploy on ghpages pt.7(lol)

--- poke-quiz/src/App.jsx
+++ poke-quiz/src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css'
-import WelcomePage from './components/WelcomePage';
-import QuizPage from './components/QuizPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import WelcomePage from "./components/WelcomePage";
+import QuizPage from "./components/QuizPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <Router>
       <div>
-      <Routes>
-        <Route path='/' element={<WelcomePage/>} />
-        <Route path='/quiz' element={<QuizPage/>} />
-      </Routes>
+        <Routes>
+          <Route path="/poke-quiz" element={<WelcomePage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+        </Routes>
       </div>
-    </Router>      
+    </Router>
   );
-
 }
 
-export default App
+export default App;
