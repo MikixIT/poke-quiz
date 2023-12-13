9919bc4196a83080a5a763ca897df866b609310f
MikixIT <michael.46@live.it>
2023-12-13 20:09:47 +0100

    update code  router react dom for gh pages adding hashrouter

--- poke-quiz/src/App.jsx
+++ poke-quiz/src/App.jsx
@@ -1,19 +1,17 @@
 import "./App.css";
 import WelcomePage from "./components/WelcomePage";
 import QuizPage from "./components/QuizPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/poke-quiz" element={<WelcomePage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/Quiz" element={<QuizPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/poke-quiz" element={<WelcomePage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/Quiz" element={<QuizPage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
