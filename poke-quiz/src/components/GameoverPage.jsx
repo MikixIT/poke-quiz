cfbc48449133c883c24829269eaca1953f0a78de
Michael Torres <michael.46@live.it>
2025-04-11 01:05:23 +0200

    fix leaderboard comp

--- poke-quiz/src/components/GameoverPage.jsx
+++ poke-quiz/src/components/GameoverPage.jsx
@@ -32,7 +32,7 @@ export default function GameoverPage() {
       <h2 className="text-4xl cursor-pointer font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient drop-shadow-xl">
         Your score: {pokeScore}
       </h2>
-      <Leaderboard></Leaderboard>
+      <Leaderboard positionsAvailable="1"></Leaderboard>
       <Link to="/Quiz">
         <button className="text-5xl relative -top-1 -left-1 bg-red-600 py-2.5 px-5 font-medium uppercase text-white transition-all before:absolute before:top-1 before:left-1 before:-z-[1] before:h-full before:w-full before:border-2 before:border-red-800 before:transition-all before:content-[''] hover:top-0 hover:left-0 before:hover:top-0 before:hover:left-0">
           Play Again!
--- poke-quiz/src/components/Leaderboard.jsx
+++ poke-quiz/src/components/Leaderboard.jsx
@@ -1,41 +1,74 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-function Leaderboard() {
+function Leaderboard({ positionsAvailable }) {
   const [leaderboard, setLeaderboard] = useState([]);
 
   useEffect(() => {
     fetch("data/score.json")
       .then((response) => response.json())
-      .then((data) => setLeaderboard(data))
+      .then((initialLeaderboard) => {
+        const storedLeaderboard = localStorage.getItem("leaderboard");
+        if (storedLeaderboard) {
+          const localLeaderboard = JSON.parse(storedLeaderboard);
+          setLeaderboard(
+            mergeLeaderboards(initialLeaderboard, localLeaderboard)
+          );
+        } else {
+          setLeaderboard(initialLeaderboard);
+        }
+      })
       .catch((error) =>
-        console.error("Can't load data Can't load data correctly", error)
+        console.error("Can't load leaderboard data correctly", error)
       );
   }, []);
+
+  const mergeLeaderboards = (initialLeaderboard, localLeaderboard) => {
+    const combinedLeaderboard = [...initialLeaderboard, ...localLeaderboard];
+    combinedLeaderboard.sort((a, b) => b.score - a.score);
+    return combinedLeaderboard.slice(0, positionsAvailable);
+  };
+
+  const addScore = (name, score) => {
+    const updatedLeaderboard = [...leaderboard, { name, score }];
+    const sortedLeaderboard = updatedLeaderboard.sort(
+      (a, b) => b.score - a.score
+    );
+    const limitedLeaderboard = sortedLeaderboard.slice(0, positionsAvailable);
+
+    setLeaderboard(limitedLeaderboard);
+    localStorage.setItem("leaderboard", JSON.stringify(limitedLeaderboard));
+  };
 
   return (
     <div className="m-6">
       <h1 className="m-2 text-2xl">🌟 Leaderboard </h1>
       <ul>
-        {leaderboard
-          .sort((a, b) => b.score - a.score)
-          .slice(0, 3)
-          .map((item, index) => (
-            <li
-              key={index}
-              className={
-                index === 0
-                  ? "text-2xl cursor-pointer font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient drop-shadow-xl"
-                  : index === 1
-                  ? "text-xl cursor-pointer font-bold bg-gradient-to-r from-orange-700 via-red-500-500 to-red-200-400 text-transparent bg-clip-text animate-gradient drop-shadow-xl "
-                  : "text-xl cursor-pointer font-bold bg-gradient-to-r from-blue-700 via-blue-500-500 to-blue-200-400 text-transparent bg-clip-text animate-gradient drop-shadow-xl "
-              }
-            >
-              {item.name}: {item.score}
-            </li>
-          ))}
+        {leaderboard.map((item, index) => (
+          <li
+            key={index}
+            className={
+              index === 0
+                ? "text-2xl cursor-pointer font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient drop-shadow-xl"
+                : index === 1
+                ? "text-xl cursor-pointer font-bold bg-gradient-to-r from-orange-700 via-red-500 to-red-200 text-transparent bg-clip-text animate-gradient drop-shadow-xl"
+                : "text-xl cursor-pointer font-bold bg-gradient-to-r from-blue-700 via-blue-500 to-blue-200 text-transparent bg-clip-text animate-gradient drop-shadow-xl"
+            }
+          >
+            {item.name}: {item.score}
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
+
+Leaderboard.propTypes = {
+  positionsAvailable: PropTypes.number.isRequired,
+};
+
+Leaderboard.defaultProps = {
+  positionsAvailable: 3, // Valore predefinito
+};
 
 export default Leaderboard;
